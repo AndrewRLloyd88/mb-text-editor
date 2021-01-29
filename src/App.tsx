a2169ada36211fe0a3f5b53acc8ebb72851355a8
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-01-29 14:57:45 -0800

    add check in useEffect to return innerHTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import * as React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 
 function App(props: any) {
   const { name } = props;
+  const content = useRef<HTMLDivElement>(null);
+  const renderToString = () => {};
+
+  useEffect(() => {
+    const el: any = content.current;
+    console.log(el.innerHTML);
+  }, []);
+
   return (
     <>
       <h1>Wysiwyg Text Editor</h1>
-      <Button variant="contained">
-        <b>B</b>
-      </Button>
-      <div className="editor" contentEditable="true">
-        <p>Edit this content to add your own quote</p>
+      <div className="toolbar">
+        <Button variant="contained">
+          <b>B</b>
+        </Button>
+      </div>
+      <div
+        className="editor"
+        contentEditable="true"
+        spellCheck="true"
+        suppressContentEditableWarning={true}
+      >
+        <p ref={content}>Edit this content to add your own quote</p>
       </div>
     </>
   );
